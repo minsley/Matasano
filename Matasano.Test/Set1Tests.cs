55caf0164503f036ca8bacb456de08a800bdb3b1
minsley <insley.matthew@gmail.com>
2014-08-12 16:42:01 -0500

    Completed through Set1 Challenge 3

---
+++ Matasano.Test/Set1Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matasano.Test
+{
+    [TestClass]
+    public class Set1Tests
+    {
+        // Convert Hex to Base64
+        [TestMethod]
+        public void TestS1C1()
+        {
+            // Hex -> Ascii "I'm killing your brain like a poisonous mushroom" :D
+            const string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            const string target = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+            Assert.AreEqual(target, Basic.HexToBase64(input));
+        }
+
+        // Xor the input and fixed val to get the target
+        [TestMethod]
+        public void TestS1C2()
+        {
+            const string input = "1c0111001f010100061a024b53535009181c";
+            const string fixedXor = "686974207468652062756c6c277320657965";
+            const string target = "746865206b696420646f6e277420706c6179";
+
+            var inputByteArray = Basic.HexToByteArray(input);
+            var fixedXorByteArray = Basic.HexToByteArray(fixedXor);
+
+            var xor = Basic.Xor(inputByteArray, fixedXorByteArray);
+            Assert.AreEqual(Basic.ByteArrayToHex(xor).ToUpper(), target.ToUpper());
+        }
+
+        [TestMethod]
+        public void TestS1C3()
+        {
+            const string cipherText = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+            var tests = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var results = new List<Tuple<double, char, string>>();
+
+            for (var i = 0; i < tests.Length; i++)
+            {
+                var key = ((int) tests[i]).ToString("X");
+                var keyBytes = Basic.HexToByteArray(key);
+                var cipherTextBytes = Basic.HexToByteArray(cipherText);
+
+                var plainBytes = Basic.Decipher(cipherTextBytes, keyBytes);
+
+                var plainText = Basic.ByteArrayToAscii(plainBytes);
+                var score = Basic.IsEnglish(plainText);
+
+                results.Add(new Tuple<double, char, string>(score, tests[i], plainText));
+            }
+
+            results.Sort((firstPair, nextPair) => { return nextPair.Item1.CompareTo(firstPair.Item1); });
+            foreach (var result in results)
+            {
+                Console.WriteLine("{0:P1}% - key: {1} - string: {2}", result.Item1, result.Item2, result.Item3.Replace("\n","\\n"));
+            }
+        }
+    }
+}
---
+++ Matasano/Basic.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
+
+namespace Matasano
+{
+    public static class Basic
+    {
+
+        public static byte[] AsciiToByteArray(string ascii)
+        {
+            return Encoding.ASCII.GetBytes(ascii);
+        }
+
+        public static string ByteArrayToAscii(byte[] byteArray)
+        {
+            return Encoding.ASCII.GetString(byteArray);
+        }
+
+        public static string ByteArrayToHex(byte[] byteArray)
+        {
+            return BitConverter.ToString(byteArray).Replace("-", "");
+        }
+
+        public static string ByteArrayToBase64(byte[] byteArray)
+        {
+            return Convert.ToBase64String(byteArray);
+        }
+
+        public static byte[] HexToByteArray(string hexInput)
+        {
+            if (hexInput.Length%2 != 0) hexInput = "0" + hexInput;
+            var byteArray = new byte[hexInput.Length/2];
+            for (var i = 0; i < hexInput.Length/2; i++)
+            {
+                byteArray[i] = Convert.ToByte(hexInput.Substring(i*2, 2), 16);
+            }
+            return byteArray;
+        }
+
+        public static string HexToBase64(string hexInput)
+        {
+            return ByteArrayToBase64(HexToByteArray(hexInput));
+        }
+
+        public static byte[] Xor(byte[] buf1, byte[] buf2)
+        {
+            if (buf1.Length != buf2.Length) throw new Exception("Inputs must be equal length.");
+            var xor = new byte[buf1.Length];
+            for (var i = 0; i < buf1.Length; i++)
+            {
+                xor[i] = Convert.ToByte(buf1[i] ^ buf2[i]);
+            }
+            return xor;
+        }
+
+        public static byte[] Decipher(byte[] cipher, byte[] key)
+        {
+            var keyRepeat = new byte[cipher.Length];
+            for (var i = 0; i < cipher.Length; i++) keyRepeat[i] = key[i % key.Length];
+            return Xor(cipher, keyRepeat);
+        }
+
+        /// <summary>
+        /// Uses character frequency to estimate the probability that the text provided is written in English.
+        /// </summary>
+        /// <param name="plainText">A string of text.</param>
+        /// <returns>0 to 1 probability of input text being English.</returns>
+        public static double IsEnglish(string plainText)
+        {
+            var plainTextContents = new Dictionary<char, int>();
+
+            // source: http://en.wikipedia.org/wiki/Letter_frequency
+            var englishCharacterFrequencies = new Dictionary<char, double>
+            {
+                {'a',0.08167}, {'b',0.01492}, {'c',0.02782}, {'d',0.04253}, {'e',0.13000},
+                {'f',0.02228}, {'g',0.02015}, {'h',0.06094}, {'i',0.06966}, {'j',0.00153},
+                {'k',0.00772}, {'l',0.04025}, {'m',0.02406}, {'n',0.06749}, {'o',0.07507},
+                {'p',0.01929}, {'q',0.00095}, {'r',0.05987}, {'s',0.06327}, {'t',0.09056},
+                {'u',0.02758}, {'v',0.00978}, {'w',0.02360}, {'x',0.00150}, {'y',0.01974},
+                {'z',0.00074}
+            };
+
+            foreach (var character in plainText.ToLower())
+            {
+                if (plainTextContents.ContainsKey(character))
+                    plainTextContents[character] += 1;
+                else
+                    plainTextContents.Add(character, 1);
+            }
+
+            var score = 0d;
+            foreach (var character in plainTextContents)
+            {
+                if (englishCharacterFrequencies.ContainsKey(character.Key))
+                {
+                    var weight = character.Value / (double)plainText.Length;
+                    var ideal = englishCharacterFrequencies[character.Key];
+                    score += ideal * (1 - Math.Abs(weight - ideal));
+                }
+            }
+
+            return score;
+        }
+    }
+}
