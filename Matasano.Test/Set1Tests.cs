625f91007ebc1f3ee0d969e048e77048222dd272
minsley <insley.matthew@gmail.com>
2014-08-13 01:34:39 -0500

    S1C4 but not really
    Oh god... I broke everything.

--- Matasano.Test/Set1Tests.cs
+++ Matasano.Test/Set1Tests.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Matasano.Test
@@ -62,11 +57,61 @@ namespace Matasano.Test
                 results.Add(new Tuple<double, char, string>(score, tests[i], plainText));
             }
 
-            results.Sort((firstPair, nextPair) => { return nextPair.Item1.CompareTo(firstPair.Item1); });
+            results.Sort((firstPair, nextPair) => nextPair.Item1.CompareTo(firstPair.Item1));
             foreach (var result in results)
             {
                 Console.WriteLine("{0:P1}% - key: {1} - string: {2}", result.Item1, result.Item2, result.Item3.Replace("\n","\\n"));
             }
+        }
+
+        [TestMethod]
+        public void TestS1C4()
+        {
+            const string path = @"..\..\Assets\S1C4.txt";
+
+            string cipher;
+
+            using (var r = new StreamReader(path))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    List<Tuple<double,byte,char>> matches;
+                    var score = Basic.IsEnglishDistributed(Basic.HexToByteArray(line), out matches);
+                    
+                    Console.WriteLine("{0:P} - {1}", score, line);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestIsEnglishDistributed()
+        {
+            const string cipherText = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+            var bytes = Basic.HexToByteArray(cipherText);
+            var matches = new List<Tuple<double, byte, char>>();
+
+            var score = Basic.IsEnglishDistributed(bytes, out matches);
+
+            Console.WriteLine("-- Score: {0:P} English --", score);
+            foreach (var match in matches)
+            {
+                Console.WriteLine("Score: {0:P} - Char: {1} - Byte: {2} - Xor: {3}", 
+                    match.Item1, match.Item3, 
+                    Basic.ByteArrayToAscii(new []{match.Item2}),
+                    Basic.ByteArrayToHex(Basic.Xor(Basic.AsciiToByteArray(match.Item3 + ""),new []{match.Item2})));
+            }
+
+            var first = matches.First();
+            var key = Basic.Xor(new[] { first.Item2 }, Basic.AsciiToByteArray("X"));//first.Item3 + ""));
+            var keyChar = Basic.ByteArrayToAscii(key);
+
+            Console.WriteLine("\n-- Key: {0}[{1}] --", Basic.ByteArrayToHex(key), keyChar);
+            Console.WriteLine("Original: " + cipherText);
+            var transbytes = Basic.Decipher(Basic.HexToByteArray(cipherText), key);
+            Console.WriteLine("Deciphered: " + Basic.ByteArrayToHex(transbytes));
+            Console.WriteLine("Plaintext: " + Basic.ByteArrayToAscii(transbytes));
         }
     }
 }
--- Matasano/Basic.cs
+++ Matasano/Basic.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Numerics;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Matasano
 {
@@ -80,7 +81,7 @@ namespace Matasano
                 {'k',0.00772}, {'l',0.04025}, {'m',0.02406}, {'n',0.06749}, {'o',0.07507},
                 {'p',0.01929}, {'q',0.00095}, {'r',0.05987}, {'s',0.06327}, {'t',0.09056},
                 {'u',0.02758}, {'v',0.00978}, {'w',0.02360}, {'x',0.00150}, {'y',0.01974},
-                {'z',0.00074}
+                {'z',0.00074},
             };
 
             foreach (var character in plainText.ToLower())
@@ -102,6 +103,66 @@ namespace Matasano
                 }
             }
 
+            return score;
+        }
+
+        /// <summary>
+        /// Estimates how close a given string (as byte array) is to being English distributed.
+        /// </summary>
+        /// <param name="bytes">String represented as byte array</param>
+        /// <param name="matches">List of tuples of (weight difference, byte-word, character)</param>
+        /// <returns>0 to 1 distribution.</returns>
+        public static double IsEnglishDistributed(byte[] bytes, out List<Tuple<double, byte, char>> matches)
+        {
+            var byteWords = new Dictionary<byte, int>();
+
+            // source: http://en.wikipedia.org/wiki/Letter_frequency
+            var englishCharacters = new Dictionary<char, double>
+            {
+                {'a',0.08167}, {'b',0.01492}, {'c',0.02782}, {'d',0.04253}, {'e',0.13000},
+                {'f',0.02228}, {'g',0.02015}, {'h',0.06094}, {'i',0.06966}, {'j',0.00153},
+                {'k',0.00772}, {'l',0.04025}, {'m',0.02406}, {'n',0.06749}, {'o',0.07507},
+                {'p',0.01929}, {'q',0.00095}, {'r',0.05987}, {'s',0.06327}, {'t',0.09056},
+                {'u',0.02758}, {'v',0.00978}, {'w',0.02360}, {'x',0.00150}, {'y',0.01974},
+                {'z',0.00074},
+            };
+
+            // Build list of byte-words and their counts
+            foreach (var b in bytes)
+            {
+                if (byteWords.ContainsKey(b))
+                    byteWords[b] += 1;
+                else
+                    byteWords.Add(b, 1);
+            }
+
+            // Build ordered list of weight differences
+            var stack = (
+                from b in byteWords 
+                from c in englishCharacters 
+                select 
+                    new Tuple<double, byte, char>(
+                        Math.Abs((double) b.Value/bytes.Length - c.Value), 
+                        b.Key, 
+                        c.Key)).ToList();
+            stack.Sort((first, next) => first.Item1.CompareTo(next.Item1));
+
+            // Take the best matches, one for each english character
+            matches = new List<Tuple<double, byte, char>>();
+            while (stack.Count > 0)
+            {
+                var top = stack[0]; stack.RemoveAt(0);
+                if(matches.All(x => x.Item3 != top.Item3 && x.Item2 != top.Item2))
+                    matches.Add(top);
+            }
+
+            // Finally, score the englishiness of the input
+            var score = 0d;
+            foreach (var match in matches)
+            {
+                var ideal = englishCharacters[match.Item3];
+                score += (ideal * (1 - match.Item1));
+            }
             return score;
         }
     }
